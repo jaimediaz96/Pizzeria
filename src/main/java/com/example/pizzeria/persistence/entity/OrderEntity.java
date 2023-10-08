aff526ce8a48264be7cddea0cde6418ff40067c7
JaimeDiaz <jedt1996@gmail.com>
2023-10-08 18:47:08 -0500

    Entity order

---
+++ src/main/java/com/example/pizzeria/persistence/entity/OrderEntity.java
@@ -0,0 +1,40 @@
+package com.example.pizzeria.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pizza_order")
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private String idCustomer;
+
+    @Column(nullable = false, columnDefinition = "timestamp")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "decimal(6,2)")
+    private Double total;
+
+    @Column(nullable = false, columnDefinition = "char(1)")
+    private String method;
+
+    @Column(name = "additional_notes", length = 200)
+    private String additionalNotes;
+}
