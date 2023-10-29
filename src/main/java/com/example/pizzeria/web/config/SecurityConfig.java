30a8f96e41cbb1315e3a89375fa4a8d266fc2bbe
JaimeDiaz <jedt1996@gmail.com>
2023-10-29 14:00:51 -0500

    Authorities

--- src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
             customizeRequests
                     .requestMatchers(HttpMethod.GET,"/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
                     .requestMatchers(HttpMethod.POST,"/api/pizzas/**").hasRole("ADMIN")
+                    .requestMatchers("/api/orders/random").hasAuthority("random_order")
                     .requestMatchers("/api/orders/**").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.PUT).denyAll()
                     .anyRequest().authenticated();
