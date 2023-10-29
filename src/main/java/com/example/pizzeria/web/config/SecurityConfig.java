4b379699df7443ebde0cd23312c540179a1d6c87
JaimeDiaz <jedt1996@gmail.com>
2023-10-29 13:24:54 -0500

    Authentication Security

--- src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +16,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(customizeRequests -> {
             customizeRequests
-                    .anyRequest()
-                    .authenticated();
-
+                    .requestMatchers(HttpMethod.GET,"/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+                    .requestMatchers(HttpMethod.POST,"/api/pizzas/**").hasRole("ADMIN")
+                    .requestMatchers("/api/orders/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.PUT).denyAll()
+                    .anyRequest().authenticated();
         })
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
                 .cors(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
