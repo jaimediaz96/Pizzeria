5092d8cc0f8a90378d9e0a128def50174e22956a
JaimeDiaz <jedt1996@gmail.com>
2023-10-28 19:05:58 -0500

    Spring Security CORS and CSRF diable

---
+++ src/main/java/com/example/pizzeria/web/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.pizzeria.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*")); // Permitira todos los encabezados a traves de CORS
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration); // Para todos los controladores
+
+        return source;
+    }
+}
